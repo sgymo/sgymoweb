5eb0b2714c5136d76f25b91d82ab86c4f0ba66ee
sgymo <33305373+sgymo@users.noreply.github.com>
2017-11-15 22:50:11 -0500

    login changes
    use media query for responsive and changes sizes.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,61 +18,63 @@
 
 	<div class="body">
 
-		<img src="{{asset('/images/login/background-body/fitness.jpg')}}" class="image-responsive image-body">
+		<img src="{{asset('/images/login/background-body/fitness1.jpg')}}" class="image-responsive image-body">
 
-		<div class="col-lg-8 col-md-7 col-sm-6 col-xs-5" style="top: 280px; padding-left: 11.75%">
-			<p class="name-login">Sgymo</p>
-			<p class="subname-login">Administra tu gimnasio</p>
+		<div class="row login-content-one">
+
+			<div class="col-lg-9 col-md-8 col-xs-8 col-sm-8"> </div>
+
+			<div class="col-lg-3 col-md-4 col-sm-4 hidden-xs column-color"> </div>
+
 		</div>
 
-		<div class="col-lg-4 col-md-5 col-sm-6 col-xs-7 column-blue">
+		<div class="row login-content-second">
 
-		</div>
+			<div class="col-lg-9 col-md-8 col-xs-8 col-sm-8 hidden-xs column-name-login"> 
+				<p class="name-login">Sgymo</p>
+				<p class="subname-login"> Administra tu gimnasio</p>
+			</div>
 
-		<div class="col-lg-4 col-md-5 col-sm-6  col-xs-7 pull-right column-login">
+			<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 login-column">
 
-			<div class="mdl-card mdl-shadow--2dp cards-login" style="">
+				<div class="mdl-card mdl-shadow--3dp login-card">
 
-				<div class="user-default">
-					<h1 class="title-login">Sgymo</h1>
-					<h6 class="subtitle-login">Iniciar sesión</h6>
+
+					<form action="#" class="form">
+
+					  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label div-input-form">
+					    <input class="mdl-textfield__input" type="email" id="email">
+					    <label class="mdl-textfield__label" for="email">Correo</label>
+					  </div>
+
+					  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label div-input-form">
+					    <input class="mdl-textfield__input" type="password" id="password">
+					    <label class="mdl-textfield__label" for="password">Contraseña</label>
+					  </div>
+
+						<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored btn-submit-login" type="submit">Ingresar</button>
+
+						<input type="hidden"  name="_token" id="token" value="{{csrf_token()}}">
+
+					</form>
+
+					<form  class="go-sign-up">
+
+						<button class="mdl-button mdl-js-button mdl-js-ripple-effect">
+	  						Registrarse
+						</button>
+
+						<input type="hidden"  name="_token" id="token" value="{{csrf_token()}}">
+
+					</form>
+			
 				</div>
 
-				<form action="#" class="form">
-
-				  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label div-input-form">
-				    <input class="mdl-textfield__input" type="email" id="email">
-				    <label class="mdl-textfield__label" for="email">Correo</label>
-				  </div>
-
-				  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label div-input-form">
-				    <input class="mdl-textfield__input" type="password" id="password">
-				    <label class="mdl-textfield__label" for="password">Contraseña</label>
-				  </div>
-
-					<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored btn-submit-login" type="submit">Ingresar</button>
-
-					<input type="hidden"  name="_token" id="token" value="{{csrf_token()}}">
-
-				</form>
-
-				<form  class="go-sign-up">
-
-					<button class="mdl-button mdl-js-button mdl-js-ripple-effect">
-  						Registrarse
-					</button>
-
-					<input type="hidden"  name="_token" id="token" value="{{csrf_token()}}">
-
-				</form>
-			
 			</div>
-			
+
 		</div>
-	
+
 	</div>
-
-
 
 	<script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
 
